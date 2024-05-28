30bab66ac1076941d62355da9c7107d03b0a66f6
Harsh Vardhan <harsh.1vardhan123@gmail.com>
2024-05-29 00:19:29 +0530

    developing

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Imagegenerator from './Components/Imagegenerator/Imagegenerator';
+
+function App() {
+  return (
+    <div style={{width : '100%'}}>
+      <Imagegenerator/>
+    </div>
+  );
+}
+
+export default App;
